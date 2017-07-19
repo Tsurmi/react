01cf6cd70b8e9f242c97fc1a38dba7890b9b9c1e
Travis Surmi <travissurmi@Traviss-MacBook-Pro.local>
2017-07-19 12:20:22 -0700

    learned about react

---
+++ src/WebApp.js
@@ -0,0 +1,51 @@
+import React from 'react';
+
+const NavBar = (props) => {
+  return (
+    <nav className='nav'>
+      <ul className='nav-bar'>
+        {
+          props.navLinks.map(function(item){
+            return <li>{item}</li>
+          })
+        }
+      </ul>
+    </nav>
+  )
+}
+
+const Jumbtron = (props) => {
+  return (
+    <div className='jumbotron'>
+      <h3> {props.header} </h3>
+        <p>  {props.callToAction} </p>
+      <h4> {props.information} </h4>
+    </div>
+  )
+}
+
+const Section = (props) => {
+  return (
+    <div>
+      <h5>{props.sectionHeader}</h5>
+      <p> {props.info} </p>
+    </div>
+  )
+}
+
+const WebApp = () => {
+  return (
+    <div className='app-container'>
+      <NavBar navLinks={["Home", "About", "Contact", "Blog"]} />
+      <Jumbtron
+        header={"Super Duper Web App"}
+        callToAction={"I built this with React"}
+        information={"This website is about React"}
+      />
+      <Section sectionHeader={"About"} />
+      <Section sectionHeader={"Info"} />
+    </div>
+  )
+}
+
+export default WebApp;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+  import React from 'react';
+  import ReactDOM from 'react-dom';
+
+  import './styles.css';
+  import WebApp from './WebApp';
+
+  ReactDOM.render(<WebApp/>,
+    document.getElementById('root'));
